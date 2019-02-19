a2f9d64560508d0e2a3ac35c70ff5fcf7d737862
Floydlholt <32264287+Floydlholt@users.noreply.github.com>
2019-02-19 17:53:52 -0500

    update
    update

---
+++ weather-app-nodejs/routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('j', function(req, res, next) {
+  res.render('index', {'body':'', forecast: ''});
+ });
+
+module.exports = router;
+
+
+let url    = 'http://api.openweathermap.org/data/2.5/weather?q='
+let appId  = 'appid=YOUR API KEY';
+let units  = '&units=metric'; 
+var request = require('request');
+
+router.post('/weather', function(req, res, next){
+  let city = req.body.city;
+  url = url+city+"&"+appId;
+ request(url, function (error, response, body) {
+      body = JSON.parse(body);
+      if(error && response.statusCode != 200){
+        throw error;
+      }
+    let country = (body.sys.country) ? body.sys.country : '' ;
+    let forecast = "For city "+city+', country '+country;
+    res.render('index', {body : body, forecast: forecast});
+   });
+});
